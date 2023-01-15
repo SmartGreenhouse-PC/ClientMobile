3d044f2e454bcf47b956a3c28d8da3bde0a1fe57
Elena Yan <elena.yan@studio.unibo.it>
2023-01-16 00:13:47 +0100

    feat: handle greenhouse data

---
+++ app/src/main/java/it/unibo/smartgh/data/homepage/GreenhouseRepositoryImpl.java
@@ -0,0 +1,38 @@
+package it.unibo.smartgh.data.homepage;
+
+import it.unibo.smartgh.entity.parameter.ParameterType;
+import it.unibo.smartgh.entity.parameter.ParameterValue;
+import it.unibo.smartgh.entity.plant.Plant;
+import it.unibo.smartgh.viewmodel.GreenhouseViewModel;
+
+public class GreenhouseRepositoryImpl implements GreenhouseRepository {
+
+    private static final String GREENHOUSE_ID = "63af0ae025d55e9840cbc1fa";
+    private final GreenhouseRemoteDataSource greenhouseRemoteDataSource;
+    private final GreenhouseViewModel viewModel;
+
+    public GreenhouseRepositoryImpl(GreenhouseViewModel viewModel) {
+        this.viewModel = viewModel;
+        this.greenhouseRemoteDataSource = new GreenhouseRemoteDataSourceImpl(this, GREENHOUSE_ID);
+    }
+
+    @Override
+    public void initializeData() {
+        this.greenhouseRemoteDataSource.initializeData();
+    }
+
+    @Override
+    public void updatePlantInformation(Plant plant) {
+        this.viewModel.updatePlantInformation(plant);
+    }
+
+    @Override
+    public void updateParameterOptimalValues(ParameterType parameterType, Double minBrightness, Double maxBrightness, String unit) {
+        this.viewModel.updateParameterInfo(parameterType, minBrightness, maxBrightness, unit);
+    }
+
+    @Override
+    public void updateParameterValue(ParameterType parameter, ParameterValue parameterValue) {
+        this.viewModel.updateParameterValue(parameter, parameterValue);
+    }
+}
--- app/src/main/java/it/unibo/smartgh/viewmodel/GreenhouseViewModel.java
+++ app/src/main/java/it/unibo/smartgh/viewmodel/GreenhouseViewModel.java
@@ -6,13 +6,18 @@ import java.util.Map;
 
 import it.unibo.smartgh.entity.parameter.ParameterType;
 import it.unibo.smartgh.entity.parameter.ParameterValue;
+import it.unibo.smartgh.entity.plant.Plant;
 
 public interface GreenhouseViewModel {
-    void updatePlantInformation(String name, String description, String plantImg);
+    void updatePlantInformation(Plant plant);
 
     void updateParameterInfo(ParameterType parameterType, Double minBrightness, Double maxBrightness, String unit);
 
     void updateParameterValue(ParameterType parameter, ParameterValue parameterValue);
 
     LiveData<Map<ParameterType, ParameterValue>> getParameterValueLiveData();
+
+    LiveData<Map<ParameterType, String>> getOptimalParameterLiveData();
+
+    LiveData<Plant> getPlantLiveData();
 }
