c307b774f0f71e3b31c0d22ac1d982a696f0cb56
Elena Yan <elena.yan@studio.unibo.it>
2023-01-16 00:13:47 +0100

    feat: handle greenhouse data

--- app/src/main/java/it/unibo/smartgh/viewmodel/GreenhouseViewModelImpl.java
+++ app/src/main/java/it/unibo/smartgh/viewmodel/GreenhouseViewModelImpl.java
@@ -7,54 +7,76 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import it.unibo.smartgh.data.homepage.GreenhouseRepository;
+import it.unibo.smartgh.data.homepage.GreenhouseRepositoryImpl;
 import it.unibo.smartgh.entity.parameter.ParameterType;
 import it.unibo.smartgh.entity.parameter.ParameterValue;
 import it.unibo.smartgh.entity.parameter.ParameterValueImpl;
+import it.unibo.smartgh.entity.plant.Plant;
 
 public class GreenhouseViewModelImpl extends AndroidViewModel implements GreenhouseViewModel {
 
     protected final GreenhouseRepository greenhouseRepository;
     protected final MutableLiveData<Map<ParameterType, ParameterValue>> parameterValueLiveData;
+    protected final MutableLiveData<Map<ParameterType, String>> optimalValuesLiveData;
+    private final MutableLiveData<Plant> plantLiveData;
 
     public GreenhouseViewModelImpl(@NonNull Application application) {
         super(application);
-        parameterValueLiveData = new MutableLiveData<>(initializeMap());
-        greenhouseRepository = new GreenhouseRepository(this);
+        parameterValueLiveData = new MutableLiveData<>(initializeMap(new ParameterValueImpl()));
+        optimalValuesLiveData = new MutableLiveData<>(initializeMap(""));
+        greenhouseRepository = new GreenhouseRepositoryImpl(this);
+        plantLiveData = new MutableLiveData<>();
         greenhouseRepository.initializeData();
     }
 
-    private Map<ParameterType, ParameterValue> initializeMap() {
-        Map<ParameterType, ParameterValue> map = new HashMap<>();
-        map.put(ParameterType.BRIGHTNESS, new ParameterValueImpl());
-        map.put(ParameterType.HUMIDITY, new ParameterValueImpl());
-        map.put(ParameterType.SOIL_MOISTURE, new ParameterValueImpl());
-        map.put(ParameterType.TEMPERATURE, new ParameterValueImpl("63af0ae025d55e9840cbc1fa", new Date(), 0.0));
+    private <V> Map<ParameterType, V> initializeMap(V value) {
+        Map<ParameterType, V> map = new HashMap<>();
+        map.put(ParameterType.BRIGHTNESS, value);
+        map.put(ParameterType.HUMIDITY, value);
+        map.put(ParameterType.SOIL_MOISTURE, value);
+        map.put(ParameterType.TEMPERATURE, value);
         return map;
     }
 
     @Override
-    public void updatePlantInformation(String name, String description, String plantImg) {
+    public void updatePlantInformation(Plant plant) {
+        this.plantLiveData.postValue(plant);
     }
 
     @Override
     public void updateParameterInfo(ParameterType parameterType, Double minBrightness, Double maxBrightness, String unit) {
-
+        Map<ParameterType, String> map = optimalValuesLiveData.getValue();
+        if (map != null) {
+            map.put(parameterType, minBrightness + " " + unit + " - " + maxBrightness + " " + unit);
+        }
+        optimalValuesLiveData.postValue(map);
     }
 
     @Override
     public void updateParameterValue(ParameterType parameter, ParameterValue parameterValue) {
         Map<ParameterType, ParameterValue> map = parameterValueLiveData.getValue();
-        map.put(parameter, parameterValue);
+        if (map != null) {
+            map.put(parameter, parameterValue);
+        }
         parameterValueLiveData.postValue(map);
     }
 
     @Override
     public LiveData<Map<ParameterType, ParameterValue>> getParameterValueLiveData() {
         return parameterValueLiveData;
+    }
+
+    @Override
+    public LiveData<Map<ParameterType, String>> getOptimalParameterLiveData() {
+        return optimalValuesLiveData;
+    }
+
+    @Override
+    public LiveData<Plant> getPlantLiveData() {
+        return plantLiveData;
     }
 }
