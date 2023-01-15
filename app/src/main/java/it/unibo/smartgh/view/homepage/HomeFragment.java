77ff9d3265220bb556103a89f95a84534872934f
Elena Yan <elena.yan@studio.unibo.it>
2023-01-15 17:39:56 +0100

    feat(homepage): create homepage and homepage parameter layouts

--- app/src/main/java/it/unibo/smartgh/view/homepage/HomeFragment.java
+++ app/src/main/java/it/unibo/smartgh/view/homepage/HomeFragment.java
@@ -1,8 +1,43 @@
 package it.unibo.smartgh.view.homepage;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-public class HomeFragment extends Fragment{
+import it.unibo.smartgh.R;
+import it.unibo.smartgh.utility.ActivityUtilities;
+
+public class HomeFragment extends Fragment {
+
+    private Activity activity;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.homepage_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.activity = getActivity();
+        if (activity != null) {
+            ActivityUtilities.setUpToolbar((AppCompatActivity) activity, "Smart Greenhouse");
+        }
+    }
 
     /**
      * Set the next view to show.
