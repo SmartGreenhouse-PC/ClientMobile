96fc0a1c7eb57a3970bdc7d9a1126eb33300c5b2
Elena Yan <elena.yan@studio.unibo.it>
2023-01-15 19:14:24 +0100

    refactor: refactor greenhouse repository

--- app/src/main/java/it/unibo/smartgh/viewmodel/GreenhouseViewModelImpl.java
+++ app/src/main/java/it/unibo/smartgh/viewmodel/GreenhouseViewModelImpl.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import java.util.HashMap;
@@ -13,26 +14,36 @@ import it.unibo.smartgh.data.homepage.GreenhouseRepository;
 import it.unibo.smartgh.entity.parameter.ParameterType;
 import it.unibo.smartgh.entity.parameter.ParameterValue;
 
-public class HomepageViewModel extends AndroidViewModel {
+public class GreenhouseViewModelImpl extends AndroidViewModel implements GreenhouseViewModel {
 
-    private final GreenhouseRepository greenhouseRepository;
-//    private final MutableLiveData<Map<ParameterType, ParameterValue>> parameterValueLiveData;
-    private final MutableLiveData<ParameterValue> parameterValueLiveData;
+    protected final GreenhouseRepository greenhouseRepository;
+    protected final MutableLiveData<Map<ParameterType, ParameterValue>> parameterValueLiveData;
 
-    public HomepageViewModel(@NonNull Application application) {
+    public GreenhouseViewModelImpl(@NonNull Application application) {
         super(application);
-        parameterValueLiveData = new MutableLiveData<>();
+        parameterValueLiveData = new MutableLiveData<>(new HashMap<>());
         greenhouseRepository = new GreenhouseRepository(this);
+        greenhouseRepository.initializeData();
     }
 
+    @Override
     public void updatePlantInformation(String name, String description, String plantImg) {
     }
 
+    @Override
     public void updateParameterInfo(ParameterType parameterType, Double minBrightness, Double maxBrightness, String unit) {
 
     }
 
+    @Override
     public void updateParameterValue(ParameterType parameter, ParameterValue parameterValue) {
-        parameterValueLiveData.setValue(parameterValue);
+        Map<ParameterType, ParameterValue> map = parameterValueLiveData.getValue();
+        map.put(parameter, parameterValue);
+        parameterValueLiveData.setValue(map);
+    }
+
+    @Override
+    public LiveData<Map<ParameterType, ParameterValue>> getParameterValueLiveData() {
+        return parameterValueLiveData;
     }
 }
