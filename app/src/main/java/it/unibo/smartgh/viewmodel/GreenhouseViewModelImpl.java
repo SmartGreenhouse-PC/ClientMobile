0eccf06cb32f51426d652ff671434edd8709c815
Elena Yan <elena.yan@studio.unibo.it>
2023-01-15 21:47:41 +0100

    fix: fix connection

--- app/src/main/java/it/unibo/smartgh/viewmodel/GreenhouseViewModelImpl.java
+++ app/src/main/java/it/unibo/smartgh/viewmodel/GreenhouseViewModelImpl.java
@@ -7,12 +7,14 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import it.unibo.smartgh.data.homepage.GreenhouseRepository;
 import it.unibo.smartgh.entity.parameter.ParameterType;
 import it.unibo.smartgh.entity.parameter.ParameterValue;
+import it.unibo.smartgh.entity.parameter.ParameterValueImpl;
 
 public class GreenhouseViewModelImpl extends AndroidViewModel implements GreenhouseViewModel {
 
@@ -21,9 +23,18 @@ public class GreenhouseViewModelImpl extends AndroidViewModel implements Greenho
 
     public GreenhouseViewModelImpl(@NonNull Application application) {
         super(application);
-        parameterValueLiveData = new MutableLiveData<>(new HashMap<>());
+        parameterValueLiveData = new MutableLiveData<>(initializeMap());
         greenhouseRepository = new GreenhouseRepository(this);
         greenhouseRepository.initializeData();
+    }
+
+    private Map<ParameterType, ParameterValue> initializeMap() {
+        Map<ParameterType, ParameterValue> map = new HashMap<>();
+        map.put(ParameterType.BRIGHTNESS, new ParameterValueImpl());
+        map.put(ParameterType.HUMIDITY, new ParameterValueImpl());
+        map.put(ParameterType.SOIL_MOISTURE, new ParameterValueImpl());
+        map.put(ParameterType.TEMPERATURE, new ParameterValueImpl("63af0ae025d55e9840cbc1fa", new Date(), 0.0));
+        return map;
     }
 
     @Override
@@ -39,7 +50,7 @@ public class GreenhouseViewModelImpl extends AndroidViewModel implements Greenho
     public void updateParameterValue(ParameterType parameter, ParameterValue parameterValue) {
         Map<ParameterType, ParameterValue> map = parameterValueLiveData.getValue();
         map.put(parameter, parameterValue);
-        parameterValueLiveData.setValue(map);
+        parameterValueLiveData.postValue(map);
     }
 
     @Override
