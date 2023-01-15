162e06be9daef67f0d62ed732468144135323313
Elena Yan <elena.yan@studio.unibo.it>
2023-01-15 23:33:18 +0100

    feat: add status in parameter value

--- app/src/main/java/it/unibo/smartgh/entity/parameter/ParameterValue.java
+++ app/src/main/java/it/unibo/smartgh/entity/parameter/ParameterValue.java
@@ -3,43 +3,55 @@ package it.unibo.smartgh.entity.parameter;
 import java.util.Date;
 
 /**
- * Interface that represent a parameter value
+ * Interface that represent a parameter value.
  */
 public interface ParameterValue {
 
     /**
-     * Get the greenhouse id
+     * Get the greenhouse id.
      * @return the greenhouse id
      */
     String getGreenhouseId();
 
     /**
-     * Get the date when the value is sensed
+     * Get the date when the value is sensed.
      * @return the date when the value is sensed
      */
     Date getDate();
 
     /**
-     * Get the plant value
+     * Get the plant value.
      * @return the plant value
      */
     Double getValue();
 
     /**
-     * Set the greenhouse id
+     * Get the parameter status.
+     * @return the parameter status
+     */
+    String getStatus();
+
+    /**
+     * Set the greenhouse id.
      * @param greenhouseId the greenhouse id
      */
     void setGreenhouseId(String greenhouseId);
 
     /**
-     * Set the plant value date
+     * Set the plant value date.
      * @param date when the value is sensed
      */
     void setDate(Date date);
 
     /**
-     * Set the value sensed
+     * Set the value sensed.
      * @param value sensed
      */
     void setValue(Double value);
+
+    /**
+     * Set the parameter status.
+     * @param status the parameter status
+     */
+    void setStatus(String status);
 }
