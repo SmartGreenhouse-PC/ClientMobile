fa60cd0d2ba9e622c1ed6a16747ba2436bc811bb
AnnaVitali <anna.vitali4@studio.unibo.it>
2023-01-16 12:39:55 +0100

    feature: start working on adapter and recycler view

--- app/src/main/java/it/unibo/smartgh/view/MainActivity.java
+++ app/src/main/java/it/unibo/smartgh/view/MainActivity.java
@@ -1,20 +1,14 @@
 package it.unibo.smartgh.view;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import it.unibo.smartgh.R;
 import it.unibo.smartgh.utility.ActivityUtilities;
 import it.unibo.smartgh.view.homepage.HomeFragment;
+import it.unibo.smartgh.view.manualControl.ManualControlFragment;
 
-/**
- * This class represents the main activity of the application.
- */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -22,21 +16,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         if (savedInstanceState == null) {
-            ActivityUtilities.insertFragment(this, new HomeFragment(), HomeFragment.class.getSimpleName());
+            //insert first fragment.
+            ActivityUtilities.insertFragment(this, new ManualControlFragment(), ManualControlFragment.class.getSimpleName());
         }
     }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        super.onOptionsItemSelected(item);
-        if (item.isVisible()) {
-            if (item.getItemId() == android.R.id.home) {
-                onBackPressed();
-                return  true;
-            }
-        }
-        return false;
-    }
-
 }
