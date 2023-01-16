3261612dcf04b393524fcf1e0a30648562f262df
Maria <mengozzi.maria@gmail.com>
2023-01-16 18:13:35 +0100

    feat: add get last param operation

--- app/src/main/java/it/unibo/smartgh/data/operation/OperationRemoteDataSource.java
+++ app/src/main/java/it/unibo/smartgh/data/operation/OperationRemoteDataSource.java
@@ -1,5 +1,7 @@
 package it.unibo.smartgh.data.operation;
 
+
+import io.vertx.core.Future;
 import it.unibo.smartgh.entity.operation.Operation;
 
 public interface OperationRemoteDataSource {
@@ -8,4 +10,12 @@ public interface OperationRemoteDataSource {
      * @param operation the new operation performed.
      */
     void sendNewOperation(Operation operation);
+
+    /**
+     * Get the last operation performed on a parameter.
+     * @param parameter on which the last operation is performed
+     * @param id greenhouse id
+     * @return the operation
+     */
+    Future<Operation> getLastParameterOperation(String parameter, String id);
 }
--- app/src/main/java/it/unibo/smartgh/viewmodel/OperationViewModel.java
+++ app/src/main/java/it/unibo/smartgh/viewmodel/OperationViewModel.java
@@ -1,5 +1,8 @@
 package it.unibo.smartgh.viewmodel;
 
+import io.vertx.core.Future;
+import it.unibo.smartgh.entity.operation.Operation;
+
 public interface OperationViewModel {
     /**
      * Send a new operation to server.
@@ -7,4 +10,11 @@ public interface OperationViewModel {
      * @param action performed.
      */
     void sendNewOperation(String parameter, String action);
+
+    /**
+     * Get the last operation performed on a parameter.
+     * @param parameter on which the last operation is performed
+     *                  TODO change return
+     */
+    void getLastParameterOperation(String parameter);
 }
