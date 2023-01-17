ccc78a64af7f6789fcab8bf33eb8d9f1b4143643
Elena Yan <elena.yan@studio.unibo.it>
2023-01-17 13:36:23 +0100

    feat: handle back pressed

--- app/src/main/java/it/unibo/smartgh/view/MainActivity.java
+++ app/src/main/java/it/unibo/smartgh/view/MainActivity.java
@@ -1,13 +1,20 @@
 package it.unibo.smartgh.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import it.unibo.smartgh.R;
 import it.unibo.smartgh.utility.ActivityUtilities;
 import it.unibo.smartgh.view.homepage.HomeFragment;
 
+/**
+ * This class represents the main activity of the application.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,10 +22,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         if (savedInstanceState == null) {
-            //insert first fragment.
             ActivityUtilities.insertFragment(this, new HomeFragment(), HomeFragment.class.getSimpleName());
         }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.isVisible()) {
+            if (item.getItemId() == android.R.id.home) {
+                onBackPressed();
+                return  true;
+            }
+        }
+        return false;
+    }
+
 }
